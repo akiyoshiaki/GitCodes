849947c0bf5c9224ee8751264be6ab31a2738ac0
ariga <cpshunya@gmail.com>
2015-12-30 20:10:10 +0900

    add a full_title helper on Rails/sample_app

--- Rails/sample_app/test/controllers/static_pages_controller_test.rb
+++ Rails/sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  #これで変数を作って記述をまとめられる
+  def setup
+    @base_title = "サンプルアプリ"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "ホームですよ"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "ヘルプですよ"
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "概要ですよ"
+    assert_select "title", "概要 | #{@base_title}"
   end
 
 end
